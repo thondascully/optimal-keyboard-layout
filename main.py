179d40c6a64410b2891860c98f0077621d521139
thondascully <thondascully@berkeley.edu>
2022-12-13 02:57:24 -0800

    Initial program header comment

---
+++ main.py
@@ -0,0 +1,5 @@
+"""
+Prints out the keyboard layout in which every key is as far away 
+from its most common neighbor (hello, h and e are neighbors),
+accounting for the top 2000 words in the english language 
+"""
