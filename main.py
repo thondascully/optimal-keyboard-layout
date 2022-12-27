4a4ccad7cb855f3a347c7c38d4a33b3d4ef49dcd
thondascully <thondascully@berkeley.edu>
2022-12-27 13:48:13 -0800

    Remove file comments

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
 """
-Prints out the keyboard layout in which every key is as far away 
-from its most common neighbor (hello, h and e are neighbors),
-accounting for the top 2000 words in the english language 
-
-To some extent, you want the keycap of each letter in a 
-word to be far away from the neighboring letters
-
-My passwords are typically 'beacon' or 'shadow' because every 
-letter is typed using a different finger (or avoids the 
-same finger usage for 2-3 letters in a row), meaning it can 
-be typed in pretty much minimal keystroke time
-
-With dvorak, there is no guarantee (and it is almost uncommon) 
-for different fingers to be used for each neighboring letter 
-of an english word. This layout is what qwerty attempted to do, 
-but definitely is not perfect. Another solution would be to 
-include an easily accessible key on the keyboard that repeats 
-the last letter typed. Therefore, words like 'hello' can be typed
-without using the same finger for both 'l's
-
 Teo Honda-Scully | 2022
 """
 
